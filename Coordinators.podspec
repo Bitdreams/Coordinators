de383358a68f203c57132df8b56fb54640675053
Raphael Cruzeiro <raphaelcruzeiro@raphaelcruzeiro.com>
2018-05-02 05:16:20 +0100

    Fixed a few navigation bar issues

--- Coordinators.podspec
+++ Coordinators.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'Coordinators'
-  s.version          = '0.1.1'
+  s.version          = '0.1.2'
   s.summary          = 'A framework for light flexible Swift MVVM coordinators'
 
 # This description is used to generate tags and improve search results.
--- Coordinators/Classes/InternalViewController.swift
+++ Coordinators/Classes/InternalViewController.swift
@@ -14,46 +14,51 @@ public protocol InternalViewControllerDelegate: class {
 
 open class InternalViewController: UIViewController {
     public weak var delegate: InternalViewControllerDelegate?
-    
+
     private let dummy = UINavigationController()
-    
+
     open var navigationBarTintColor: UIColor {
-        return dummy.navigationBar.barTintColor ?? .gray
+        return dummy.navigationBar.barTintColor ?? .white
     }
-    
+
     open var isNavigationBarTransluscent: Bool {
         return false
     }
-    
+
     open var navigationBarTextTintColor: UIColor {
-        return dummy.navigationBar.tintColor ?? .gray
+        return dummy.navigationBar.tintColor ?? .black
     }
-    
+
     override public init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         super.init(nibName: nil, bundle: nil)
         view.backgroundColor = .white
     }
-    
+
     public required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     override open func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        navigationController?.navigationBar.tintColor = navigationBarTintColor
-        
+        navigationController?.navigationBar.barTintColor = navigationBarTintColor
+        navigationController?.navigationBar.tintColor = navigationBarTextTintColor
+        navigationController?.navigationBar.isTranslucent = isNavigationBarTransluscent
+        navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: navigationBarTextTintColor]
+        if #available(iOS 11.0, *) {
+            navigationController?.navigationBar.largeTitleTextAttributes = [.foregroundColor: navigationBarTextTintColor]
+        }
+
         if isNavigationBarTransluscent {
             navigationController?.navigationBar.backgroundColor = .clear
             navigationController?.navigationBar.shadowImage = UIImage()
             navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
         } else {
-            
             navigationController?.navigationBar.backgroundColor = dummy.navigationBar.backgroundColor
             navigationController?.navigationBar.shadowImage = dummy.navigationBar.shadowImage
             navigationController?.navigationBar.setBackgroundImage(dummy.navigationBar.backgroundImage(for: .default), for: .default)
         }
     }
-    
+
     override open func didMove(toParentViewController parent: UIViewController?) {
         if parent == nil {
             delegate?.viewControllerDidDismiss()
